016f5dd12dbaa119fc983ad8243a1b4f3bd30191
kutluhanozbakan <kutluhanozbakan21@gmail.com>
2021-01-30 11:44:21 +0300

    Creating Project

---
+++ app/src/main/java/ise308/ozbakan/vahikutluhan/ozbakan_vahitkutluhanozbakan_moviechecker/MainActivity.kt
@@ -0,0 +1,11 @@
+package ise308.ozbakan.vahikutluhan.ozbakan_vahitkutluhanozbakan_moviechecker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
